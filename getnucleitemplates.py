b7c199c0a8112cdc0c9b75deb4097b6f7a13842c
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2023-05-22 13:37:12 +0200

    Update getnucleitemplates.py
    Improved checking of valid nuclei template repos

--- getnucleitemplates.py
+++ getnucleitemplates.py
@@ -4,9 +4,8 @@
 import os
 import git
 import requests
-import fileinput
 from typing import List, Tuple
-
+import fileinput
 
 def read_urls_from_file(filepath: str) -> List[str]:
     """Read URLs from the provided text file, ignoring commented lines."""
@@ -24,28 +23,31 @@ def is_url_valid(url: str) -> bool:
         return False
 
 
-def clone_repo(url: str, index: int) -> Tuple[bool, bool, bool]:
+def clone_repo(url: str, index: int) -> Tuple[bool, bool]:
     """Attempt to clone the repository from the given URL."""
-    # Ignore if URL is a gist
-    if "gist.github.com" in url:
-        print(f"Ignoring gist: {url}")
-        return False, True, False
 
     repo_name = url.split('/')[-1]  # Extract repository name
     repo_name = f"{repo_name}_{index}"  # Append index to make it unique
 
     if not is_url_valid(url):
         print(f"URL not valid: {url}")
-        return False, False, True
+        return False, True
 
     try:
         print(f"Cloning {url} into {repo_name}")
-        git.Repo.clone_from(url, repo_name)  # Clone the repository
-        return True, False, False
+        repo = git.Repo.clone_from(url, repo_name)  # Clone the repository
+
+        # Check if the repository contains .yaml or .yml files (nuclei templates)
+        for file in repo.tree().traverse():
+            if file.path.endswith(('.yaml', '.yml')):
+                return True, False
+
+        print(f"No valid Nuclei templates found in {url}")
+        return False, False
+
     except Exception as e:
         print(f"Failed to clone {url}. Reason: {e}")
-        return False, False, False
-
+        return False, False
 
 def remove_empty_dirs() -> None:
     """Remove all empty directories in the current working directory."""
@@ -65,20 +67,17 @@ def main():
     urls = read_urls_from_file('../nuclei.txt')
 
     # Initialize counters for the total, successful, and failed attempts
-    total_attempts, successful_downloads, failed_downloads, ignored_gists, invalid_urls = 0, 0, 0, 0, 0
+    total_attempts, successful_downloads, failed_downloads, invalid_urls = 0, 0, 0, 0
 
     for index, url in enumerate(urls):
         if url.startswith('#'):  # ignore commented lines
-            ignored_gists += 1
             continue
 
         total_attempts += 1
-        success, is_gist, is_invalid = clone_repo(url, index)
+        success, is_invalid = clone_repo(url, index)
 
         if success:
             successful_downloads += 1
-        elif is_gist:
-            ignored_gists += 1
         elif is_invalid:
             invalid_urls += 1
             with open('../nuclei.txt', 'a') as f:  # Append mode
@@ -96,7 +95,6 @@ def main():
     print(f"\nTotal attempted downloads: {total_attempts}")
     print(f"Successful downloads: {successful_downloads}")
     print(f"Failed downloads: {failed_downloads}")
-    print(f"Ignored gists: {ignored_gists}")
     print(f"Ignored invalid URLs: {invalid_urls}")
 
 
